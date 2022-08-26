af2e27f6f9c18a645a47602cd6429d37670495fb
Alvaro <alvaropires.eng@hotmail.com>
2022-08-26 15:13:03 -0300

    feat: wip 3

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -149,6 +149,6 @@ INTERNAL_IPS = [
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
